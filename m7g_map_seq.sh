1c8969ada621f497f270c7d5104a7d4825469be8
jeppevinther <42373129+jeppevinther@users.noreply.github.com>
2018-08-29 21:59:07 +0200

    Update m7g_map_seq.sh

--- m7g_map_seq.sh
+++ m7g_map_seq.sh
@@ -1,12 +1,15 @@
-#m7G-MaP-seq data analysis pipeline
+#General m7G-MaP-seq data analysis pipeline
 
 ##############
 #Preparation for mapping
 
 #If necessary concatenate fastq files from the same index
+zcat file1.fastq.gz file2.fastq.gz
 
+#
+nice cutadapt -a AGATCGGAAGAGCACACGTCT --nextseq-trim=20 - 2>srna_m7g."$lab_number".R1.cutadapt.error | gzip > srna_m7g_"$lab_number"_R1.fastq.gz & 
 
-zcat file1.fastq.gz file2.fastq.gz | nice cutadapt -a AGATCGGAAGAGCACACGTCT --nextseq-trim=20 - 2>srna_m7g."$lab_number".R1.cutadapt.error | gzip > srna_m7g_"$lab_number"_R1.fastq.gz & 
+nice zcat /seqdata/krogh/jvinther/170907/FASTQ/H5KY3BGX3_casava_1_mismatches/N127/*_S"$lab_number"_L00*_R1_001.fastq.gz* | nice cutadapt -a AGATCGGAAGAGCACACGTCT --nextseq-trim=20 - 2>srna_m7g."$lab_number".R1.cutadapt.error | gzip > srna_m7g_"$lab_number"_R1.fastq.gz & 
 
 
 #Trim adapters and low quality sequences
