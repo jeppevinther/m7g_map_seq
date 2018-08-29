194fb70f1ea3c15a7ec83d3e91f6ccd973cfe65c
jeppevinther <42373129+jeppevinther@users.noreply.github.com>
2018-08-29 22:26:54 +0200

    Update m7g_map_seq.sh

--- m7g_map_seq.sh
+++ m7g_map_seq.sh
@@ -1,18 +1,32 @@
 #General m7G-MaP-seq data analysis pipeline
 
 ##############
-#Preparation for mapping
+#Preparation for mapping (single end data)
 
 #If necessary concatenate fastq files from the same index
-zcat file1.fastq.gz file2.fastq.gz
+zcat index1_file1.fastq.gz index1_file2.fastq.gz > index1.fastq.gz
 
-#
-nice cutadapt -a AGATCGGAAGAGCACACGTCT --nextseq-trim=20 - 2>srna_m7g."$lab_number".R1.cutadapt.error | gzip > srna_m7g_"$lab_number"_R1.fastq.gz & 
+#Remove adapters (depends on the method used for library preparation, here standard Illumina adapter)
+#We have used cutadapt, but other tools such as Trimmomatic can also be used 
+#For NextSeq sequencing use --nextseq-trim=20
+cutadapt -a AGATCGGAAGAGCACACGTCT --nextseq-trim=20 index1.fastq.gz 2> index1.cutadapt.error | gzip > index1_trimmed.fastq.gz
 
-nice zcat /seqdata/krogh/jvinther/170907/FASTQ/H5KY3BGX3_casava_1_mismatches/N127/*_S"$lab_number"_L00*_R1_001.fastq.gz* | nice cutadapt -a AGATCGGAAGAGCACACGTCT --nextseq-trim=20 - 2>srna_m7g."$lab_number".R1.cutadapt.error | gzip > srna_m7g_"$lab_number"_R1.fastq.gz & 
+##############
+#Mapping (single end data)
+
+#Make bowtie2 index file from Fasta file containing the sequences to be analysed
+bowtie2-build RNA_sequences.fa RNA_sequences
+
+#High sensitivity mapping with bowtie2 (single read) 
+bowtie2 --local -N 1 -D 20 -R 3 -L 15 -x RNA_sequences -U index1_trimmed.fastq.gz 2>index1_bowtie2.error | gzip > index_mapped.sam.gz
+
+##############
+#Pileup of mapped reads
 
 
-#Trim adapters and low quality sequences
+
+
+
 
 
 #
