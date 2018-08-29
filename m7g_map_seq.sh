f52a73c49f4293add5df7b425960c8f3d0421922
jeppevinther <42373129+jeppevinther@users.noreply.github.com>
2018-08-29 22:57:38 +0200

    Update m7g_map_seq.sh

--- m7g_map_seq.sh
+++ m7g_map_seq.sh
@@ -17,16 +17,48 @@ cutadapt -a AGATCGGAAGAGCACACGTCT --nextseq-trim=20 index1.fastq.gz 2> index1.cu
 #Make bowtie2 index file from Fasta file containing the sequences to be analysed
 bowtie2-build RNA_sequences.fa RNA_sequences
 
-#High sensitivity mapping with bowtie2 (single read) 
-bowtie2 --local -N 1 -D 20 -R 3 -L 15 -x RNA_sequences -U index1_trimmed.fastq.gz 2>index1_bowtie2.error | gzip > index_mapped.sam.gz
+#High sensitivity mapping with bowtie2 (single read)
+#Local alignment with shortend seed that allows for mismatches, reseeding
+bowtie2 --local -N 1 -D 20 -R 3 -L 15 -x RNA_sequences -U index1_trimmed.fastq.gz 2>index1_bowtie2.error | gzip > index1_mapped.sam.gz
 
 ##############
-#Pileup of mapped reads
+#Pileup of mapped reads using mpileup
+
+#Sorting and making bams, necessary for input into mpileup
+samtools view -u -S index1_mapped.sam.gz|samtools sort > index1.bam
+
+#prepare a file listing the the path/filenames of the indexes relevant for the analysis
+#Bam list example: index 1, 2, 3 are controls and index 4, 5, 6 are treated
+echo "" > bam_file.txt
+for lab_number in 1 2 3 4 5 6
+do
+echo path/index"$lab_number".bam >> bam_file.txt
+done
+wait
+
+#mpileup using bam list 
+samtools mpileup -A -d 300000 -f RNA_sequences.fa -b bam_file.txt > analysis.mpileup
+
+cd /binf-isilon/vintherlab/jvinther/070916/scratch/reanalysis150118/BacteriaPlant/bam_files/ara_wt_rrna
+
+
+##############
+#Parsing mpileup file with getFreq Rscript
+#Save the getFreq2000.R file to your system
+R
+
+source("path/getFreq2000.R")                           #read in the getFreq function
+
+getFreq("path/analysis.mpileup",                       #path to input mpileup file
+                     CC = c(0,0,0,1,1,1),              #definition of the samples control=0 and treated=1
+                     CSVout = "path/analysis_out.txt", #output file
+                     minFrac = 0.001,                  #minimum fraction of mutations for analysis to be performed for position
+                     minCounts = 1,                    #minimum number of counts for analysis to be performed for position
+                     pvalThres = -1,                   #test all sites even if non-variable
+                     nCores = 50,                      #
+                     nSites = 500,                     #
+                     maxSites = 500000)                #
 
 
 
 
-
-
-
-#
